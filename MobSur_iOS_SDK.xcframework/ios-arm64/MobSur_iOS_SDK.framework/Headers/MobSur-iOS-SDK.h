57356fde5b43f8a1c9bfc7ffd1e8fe4646c768d4
Lachezar Todorov <lachezar.todorov@edentechlabs.io>
2022-10-11 17:09:14 +0300

    Updated the framework with the latest debug options

--- MobSur_iOS_SDK.xcframework/ios-arm64/MobSur_iOS_SDK.framework/Headers/MobSur-iOS-SDK.h
+++ MobSur_iOS_SDK.xcframework/ios-arm64/MobSur_iOS_SDK.framework/Headers/MobSur-iOS-SDK.h
@@ -8,10 +8,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for MobSur-iOS-SDK.
-FOUNDATION_EXPORT double MobSur-iOS-SDKVersionNumber;
+FOUNDATION_EXPORT double MobSur_iOS_SDKVersionNumber;
 
 //! Project version string for MobSur-iOS-SDK.
-FOUNDATION_EXPORT const unsigned char MobSur-iOS-SDKVersionString[];
+FOUNDATION_EXPORT const unsigned char MobSur_iOS_SDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MobSur-iOS-SDK/PublicHeader.h>
 
